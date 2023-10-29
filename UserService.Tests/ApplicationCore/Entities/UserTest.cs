7eafdb291ec37753e2308036e36ea43f60742c9c
philipdaquin <philipdaquin20@gmail.com>
2023-10-29 21:56:20 +0800

    unit  tests

---
+++ UserService.Tests/ApplicationCore/Entities/UserTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UserService.Tests.ApplicationCore.Entities
+{
+    public class UserTest
+    {   
+
+        UserService
+
+
+        [Fact]
+        public void TestName()
+        {
+
+    
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
