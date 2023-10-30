0d09045f438a951e4eee30d535bda0ac8ae2dd27
philipdaquin <philipdaquin20@gmail.com>
2023-10-30 21:02:26 +0800

    integration test

---
+++ test/UserService.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace UserService.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/UserService.UnitTests/Services/UserServiceTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UserService.UnitTests.Services
+{   
+    //  To do!
+    //  
+    //
+    public class UserServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
