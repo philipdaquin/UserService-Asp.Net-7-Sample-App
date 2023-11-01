d77f48fa470ba0083ec6423926b5cddee410d6d3
philipdaquin <philipdaquin20@gmail.com>
2023-11-02 05:31:38 +0800

    improve integration tests

--- test/UserService.IntegrationTests/Repository/PostgresIntegrationTests.cs
+++ test/UserService.IntegrationTests/Repository/PostgresIntegrationTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Domains;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using UserService.Repository;
 using UserService.Repository.EfCore;
 using Xunit;
@@ -24,7 +25,7 @@ namespace UserService.IntegrationTests.Repository
         }
 
         [Fact]
-        public async Task ShouldInsert_NewUser_QueryOnSuccess()
+        public async Task shouldInsert_NewUser_QueryOnSuccess()
         {
             User userA = new User { 
                 Email = "userA@mail.com",
@@ -45,22 +46,77 @@ namespace UserService.IntegrationTests.Repository
 
 
         [Fact]
-        public async Task GetById_ShouldReturnProduct_WhenProductExist() { 
+        public async Task shouldGetById_ShouldReturnUser_WhenUserExist() { 
             
             User userA = new User { 
                 Email = "userA@mail.com",
                 Name = "userA"
             };
-
             User user = await userService.Save(userA);
             User? userTest = await userService.FindOne(user.Id);
-
             // User? test = await userContext.Users.FindAsync(user.Id);
-            
-
             Assert.Equal(user.Name, userTest?.Name);
             Assert.Equal(user.Email, userTest?.Email);
             Assert.True(true);
         }
+
+        [Fact]
+        public void shouldPartiallyUpdate_GivenOnSuccess_ReturnsUpdatedUser() {
+            User userA = new User { 
+                Email = "userA@mail.com",
+                Name = "userA"
+            };
+            User user = await userService.Save(userA);
+
+            User updatedUser = new User { 
+                Id = user.Id,
+                Email = "userB@mail.com",
+                Name = "userB"
+            };
+            User partialUpdateRes = await userService.PartialUpdate(updatedUser, user.Id);
+            Assert.NotEqual(user.Name, updatedUser?.Name);
+            Assert.NotEqual(user.Email, updatedUser?.Email);
+            Assert.Equal(user.Id, updatedUser?.Id);
+        }
+        
+        [Fact]
+        public async void shouldDeleteUser_GivenUserExists_ReturnsTrue() {
+            User userA = new User { 
+                Email = "userA@mail.com",
+                Name = "userA"
+            };
+            User user = await userService.Save(userA);
+
+            Assert.NotNull(user);
+            bool isDeleted = await userService.DeleteOne(userA.Id);
+            Assert.True(isDeleted);
+            Assert.Null(await userService.FindOne(user.Id));
+        }
+        
+        [Fact]
+        public async void shouldGetAllUsers_GivenDBisNotEmpty_ReturnsListOfUsers() {
+            User userA = new User { 
+                Email = "userA@mail.com",
+                Name = "userA"
+            };
+            User savedA = await userService.Save(userA);
+
+            User userB = new User { 
+                Email = "userB@mail.com",
+                Name = "userB"
+            };
+            User savedB = await userService.Save(userB);
+
+            // 
+            Assert.NotNull(savedA);
+            Assert.NotNull(savedB);
+
+            List<User> userList = await userService.FindAll();
+            Assert.NotNull(userList);
+            Assert.Equal(userList.Count, 2);
+        }
+
+
+
     }
 }
