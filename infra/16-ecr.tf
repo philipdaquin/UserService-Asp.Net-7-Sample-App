b8b24af8eb65c189008157fcdf0f96136a1d33d9
philipdaquin <philipdaquin20@gmail.com>
2023-11-02 11:21:35 +0800

    added force delete on ecr

--- infra/16-ecr.tf
+++ infra/16-ecr.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "ecr" {
     name = "${local.env}-ecr"
     image_tag_mutability = "MUTABLE"
     
+    force_delete = true
 
     image_scanning_configuration {
       scan_on_push = true
